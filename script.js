b410ddd88db906a56f727af06e34676a1f05e7dd
Hizbull357 <hizbullah357@gmail.com>
2024-11-19 17:06:53 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll("nav ul li a");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {});
+  });
+});
